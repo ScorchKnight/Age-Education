a99446e0867e760e2e920768669f8ebdbc407b19
Saad <saadnabi95@gmail.com>
2022-05-12 10:51:13 -0400

    May 12

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import random
+from unicodedata import name
 
 def welcome():
   print(
-    """
-    > Welcome to the Age Education"""
+    """> Welcome to the Age Education"""
   )
-  name = input("> Please type your name: ").capitalize()
+  name = input("> Please type your first name only: ").capitalize()
   if name.isalpha() == True:
-    print("> Hello", name, "Today you will play a game that lets you experience what other generation thinks of you. Through this game, we hope to give you some knowledge about people of other generation and their thoughts about you.")
+    print("> Hello " + name + "! Today you will play a game that lets you experience what people of other generation thinks of you. Through this game, we hope to give you some knowledge about people of different generation and their thoughts about you.")
   else:
     print("Please enter your name in letters only")
     print("Restart the code and try again")
 welcome()
+
